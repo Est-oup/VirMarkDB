78883b1496a6f07639099c0e0a52299085cf5ead
Est-oup <paul.estoup@sb-roscoff.fr>
2026-05-04 16:34:54 +0200

    Update hmm output format

--- scripts/analysis/05_hmmsearch.bash
+++ scripts/analysis/05_hmmsearch.bash
@@ -12,21 +12,12 @@ mkdir -p "$OUT"
 for faa in "$FAA_DIR"/*.faa; do
   genome_id="$(basename "$faa" .faa)"
 
-  grep "^${genome_id}" "$MAP" | cut -f11 | sort -u | while read -r marker_group_id; do
-
-    if [ -z "$marker_group_id" ]; then
-      continue
-    fi
+  grep "^${genome_id}" "$MAP" | cut -f11 | while read -r marker_group_id; do
 
     hmm="${HMM_DIR}/${marker_group_id}.hmm"
 
-    if [ ! -f "$hmm" ]; then
-      echo "Missing HMM: $hmm"
-      continue
-    fi
-
     echo "Genome: $genome_id | Marker: $marker_group_id"
 
-    hmmsearch -E 0.00001 --tblout "$OUT/${genome_id}__${marker_group_id}.tbl" "$hmm" "$faa" > /dev/null
+    hmmsearch -E 0.00001 --domtblout "$OUT/${genome_id}__${marker_group_id}.tbl" "$hmm" "$faa" > /dev/null
   done
 done
